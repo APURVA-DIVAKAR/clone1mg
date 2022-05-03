a754cb890133c83d428e0b7e20126283d1ec2b8b
APURVA-DIVAKAR <99614708+APURVA-DIVAKAR@users.noreply.github.com>
2022-05-03 20:22:39 +0530

    up

--- clone1mg/src/components/Signup.jsx
+++ clone1mg/src/components/Signup.jsx
@@ -1,56 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import 'react-slideshow-image/dist/styles.css'
-import { Fade } from "react-slideshow-image";
+// import { Fade } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
-import {
-    Add_div} from "../Styleing/Advertisement_style";
-  
-  import { Swiper, SwiperSlide } from "swiper/react";
-  // Import Swiper styles
-  import "swiper/css";
-  import "swiper/css/pagination";
-  import { Pagination, Autoplay } from "swiper";
 
 const Signup = () => {
     // console.log(fadeImages[0].url)
   return (
     <div style={{display: 'flex', }}>
-         <Add_div>
-        <div>
-          <Swiper
-            pagination={true}
-            modules={[Pagination, Autoplay]}
-            autoplay={{ delay: 5000 }}
-            className="mySwiper"
-          >
-            <SwiperSlide>
-              <img
-                src="https://www.1mg.com/images/login-signup/Know-Your-Medicines.png"
-                alt=""
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src="https://www.1mg.com/images/login-signup/Home-Delivery-of-Medicines.png"
-                alt=""
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src="https://www.1mg.com/images/login-signup/Health-Related-Queries.png"
-                alt=""
-              />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img
-                src="https://www.1mg.com/images/login-signup/Your-Go-To-Health-App.png"
-                alt=""
-              />
-            </SwiperSlide>
-            </Swiper>
-            </div>
-            </Add_div>
+        
         <div>
             <h1>Sign Up</h1>
             <p>Please enter your Mobile number to receive One Time Password (OTP)</p>
