b681e2e876f535958469a1c64c961fc3751cc1aa
Devesh Goyal <Devesh26goyal@gmail.com>
2022-05-07 10:41:18 +0530

    ad

--- clone1mg/src/App.js
+++ clone1mg/src/App.js
@@ -18,27 +18,32 @@ import Ayurveda from "./Ayurveda_Navbar/Ayurveda";
 import Products from "./ProductsPage/Products";
 import Address from "./Address/Address";
 import Delivery from "./Address/Delivery";
+import { Cart_com } from "./Cart/Components/Cart_com";
+import { Individual_Product_Page } from "./individual_product_page/Components/Individual_Product_Page";
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
-      <Individual_Product_Page />
-      <Cart_com />
+      <Navbar />
 
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/Cart" element={<Cart_com />}></Route>
         <Route path="/Upload" element={<Upload />}></Route>
         <Route path="/AskDoctor" element={<Ask_Doctor />}></Route>
         <Route path="/Covid" element={<Covid />}></Route>
 
         <Route path="/Ayurveda" element={<Ayurveda />}></Route>
-       
-      </Routes> */}
 
-      {/* <LastsecFooter />
+        <Route
+          path="/Products/:id"
+          element={<Individual_Product_Page />}
+        ></Route>
+      </Routes>
+
+      {/* <LastsecFooter /> */}
       <LastFooter />
-      <Products/> */}
+      {/* <Products /> */}
     </div>
   );
 }
